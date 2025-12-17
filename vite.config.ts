518d08194e68b47f767cbfd1a17d45eb2f7adbf3
Kinshuk Saxena <kinshuksaxena3@gmail.com>
2025-12-18 00:25:34 +0530

    feat: configure for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/PARSE-N-PRACTICE/",
+  base: "/", // Changed for Vercel deployment
   plugins: [react()],
   resolve: {
     alias: {
